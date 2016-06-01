66cc51a6aff8de340cb257ff030a2e2afb48c4fc
AlexBlas <alex-blas@hotmail.com>
2016-06-01 13:22:20 +0200

    Update MainTest.java

--- MainTest.java
+++ MainTest.java
@@ -8,7 +8,7 @@ public class MainTest {
 	@Test
 	public void testNova_lletra() {
 		
-		Main.p ="hola"; //Seleccionar la "hola";
+		Main.p ="hola"; //Seleccionar la paraula "hola";
 		
 		assertEquals(true,Main.nova_lletra('h')); //Comproba si en la paraula "hola" te la lletra 'h', com es cert retorna true.
 		assertEquals(false,Main.nova_lletra('p')); //Comproba si en la paraula "hola" te la lletra 'p' com es fals retorna false.
