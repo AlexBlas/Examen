461975fc1bffa12fda00ee8d51c250be73f6af7f
AlexBlas <alex-blas@hotmail.com>
2016-06-01 12:50:04 +0200

    Codi jUnit

---
+++ MainTest.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MainTest {
+
+
+	@Test
+	public void testNova_lletra() {
+		
+		Main.p ="hola"; //Seleccionar la "hola";
+		
+		assertEquals(true,Main.nova_lletra('h')); //Comproba si en la paraula "hola" te la lletra 'h', com es cert retorna true.
+		assertEquals(false,Main.nova_lletra('p')); //Comproba si en la paraula "hola" te la lletra 'p' com es fals retorna false.
+	}
+
+}
